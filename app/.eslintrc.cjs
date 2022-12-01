ef8870afb4de59d49b5de115e11e998ae0dc112d
Patricia Andreica <elisaandreica@gmail.com>
2022-12-01 16:11:39 +0000

    close #3 code quality rules

---
+++ app/.eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'eslint-config-prettier'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  globals: {
+    __APP_VERSION__: 'readonly',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+  rules: {
+    // override/add rules settings here, such as:
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'warn',
+    // 'empty-brace-spaces': 'warn', //Enforce no spaces between braces.
+  },
+};
