29e53b8775af8a60cee949be9b29c6a0eb72594a
Patricia Andreica <peandreica1116@eagle.fgcu.edu>
2023-02-15 15:17:12 -0500

    rendered info under table component, added some styling for now

--- app/src/SelectedUser.jsx
+++ app/src/SelectedUser.jsx
@@ -6,16 +6,34 @@ const SelectedUser = (props) => {
     const { parentUsers } = props;
     const [selectedUser, setSelectedUser] = useState(null);
 
-
     return (
         <div>
             <h3>Customer Health:</h3>
             <p> {parentUsers}</p>
+            <table style={{ borderCollapse: 'collapse', width: '100%', marginBottom: '20px' }}>
+                <thead>
+                    <tr>
+                        <th style={{ backgroundColor: "#B6D770", color: '#555', fontWeight: 'bold', textTransform: 'uppercase', padding: '8px', textAlign: 'left' }}>{parentUsers}</th>
+                        <th style={{ backgroundColor: "#B6D770", color: '#555', fontWeight: 'bold', textTransform: 'uppercase', padding: '8px', textAlign: 'left' }}>{parentUsers}</th>
+                        <th style={{ backgroundColor: "#B6D770", color: '#555', fontWeight: 'bold', textTransform: 'uppercase', padding: '8px', textAlign: 'left' }}>{parentUsers}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>{parentUsers}</td>
+                        <td style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>{parentUsers}</td>
+                        <td style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>{parentUsers}</td>
+                    </tr>
+                    <tr>
+                        <td style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd', backgroundColor: '#f2f2f2' }}>{parentUsers}</td>
+                        <td style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd', backgroundColor: '#f2f2f2' }}>{parentUsers}</td>
+                        <td style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd', backgroundColor: '#f2f2f2' }}>{parentUsers}</td>
+                    </tr>
+                </tbody>
+            </table>
+
         </div>
-        //stats
-
-    )
-
+    );
 }
 
 export default SelectedUser;
