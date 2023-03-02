2643a7d3689efd90142c0e6eb4ac9da6d8279b86
Patricia Andreica <peandreica1116@eagle.fgcu.edu>
2023-03-01 20:10:49 -0500

    fetched total testimonials data

--- app/src/pages/UserPage.jsx
+++ app/src/pages/UserPage.jsx
@@ -1,15 +1,36 @@
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import { useEffect, useState } from 'react';
 
 import './UserPage.css';
 import { Card, CardContent, CardMedia } from '@mui/material';
 
 export default function UserPage({ user, onClearUser }) {
+    const [testimonialCount, setTestimonialCount] = useState([]);
+    const [weeklyTestimonialCount, setWeeklyTestimonialCount] = useState([]);
+    const [userID, setUserID] = useState(user.userId);
+
+    console.log(userID);
+
+    // Total Testimonals Count
+    useEffect(() => {
+        fetch(`https://localhost:7107/api/v1/data/${userID}/testimonialcount`)
+            .then((response) => response.text())
+            .then((text) => setTestimonialCount(parseInt(text) || 0))
+    }, [userID]);
+
+    // Weekly Testimonials Count -- In progress
+    useEffect(() => {
+        fetch(`https://localhost:7107/api/v1/data/${userID}/weeklytestimonial`)
+            .then((response) => response.text())
+            .then((text) =>  setWeeklyTestimonialCount(parseInt(text) || 0))
+    }, [userID]);
+
   const handleHomeClick = (event) => {
     event.preventDefault();
     onClearUser();
-  };
+    };
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
@@ -28,21 +49,21 @@ export default function UserPage({ user, onClearUser }) {
         </Link>
         <Link color="inherit" href="/parentUsers">
           {user.username}
-        </Link>
+                </Link>
           </Breadcrumbs>
             {/* Title */}
-          <div className="customer-health-heading">
-              <span>{user.username} Dashboard</span>
+            <div className="customer-health-heading">
+                <span>{user.username} Dashboard </span>
           </div>
             {/* Stats Cards */}
           <div style={{ display: 'flex' }}>
                 <Card className="card" style={{ borderRadius: '20px', backgroundColor: '#0080001c' }}>
                     <CardContent className="content" style={{} }>
                         <Typography variant="h1" className="card-info">
-                            75
+                            {testimonialCount}
                         </Typography>
                         <Typography variant="h5" component="h2" className="title">
-                            Average Star Rating
+                            Total Testimonials
                         </Typography>
                     </CardContent>
                 </Card>
@@ -50,7 +71,7 @@ export default function UserPage({ user, onClearUser }) {
                 <Card className="card" style={{ borderRadius: '20px',backgroundColor: '#f9f9f9' }}>
                     <CardContent className="content" style={{ }}>
                         <Typography variant="h1" className="card-info">
-                            20
+                            {weeklyTestimonialCount}
                         </Typography>
                         <Typography variant="h5" component="h2" className="title">
                             Last Week's Reviews
